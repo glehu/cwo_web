31d36565de264e4ed11f709475d726343c45cb93
glehu <duffystar1@gmail.com>
2021-12-30 23:11:21 +0100

    Fixed bug

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  module: {
-    rules: [{
-      test: /\.(woff|woff2|eot|ttf|svg)(\?.*$|$)/,
-      loader: 'file-loader'
-    }]
-  }
+  ]
 }
