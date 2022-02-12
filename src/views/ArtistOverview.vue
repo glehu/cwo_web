2c1166e2091ad3b7955249cc9ff4c7e4e7d06bbe
glehu <duffystar1@gmail.com>
2022-02-13 00:41:16 +0100

    Updated ArtistOverview.vue

--- src/views/ArtistOverview.vue
+++ src/views/ArtistOverview.vue
@@ -1,31 +1,127 @@
 <template>
-  <div class="supercenter text-center" style="width: 350px">
-    <!-- Icons -->
-    <div class="mb-4" style="display: flex; justify-content: space-evenly">
-      <div class="doHover">
-        <i class="bi bi-search text-white h1 mx-2"/>
+  <div class="container">
+    <div style="min-height: 10ch"></div>
+    <div class="wrapper">
+      <!-- Artist Hub Welcome Message -->
+      <div class="text-center">
+        <div class="mb-4" style="display: flex; justify-content: space-evenly">
+          <div class="doHover">
+            <i class="bi bi-search text-white h1 mx-2"/>
+          </div>
+          <div class="doHover2">
+            <i class="bi bi-book text-white h1 mx-2"/>
+          </div>
+          <div class="doHover3">
+            <i class="bi bi-lightning text-white h1 mx-2"/>
+          </div>
+        </div>
+        <h1 class="text-white mb-4 fw-bold">Discover. Learn. Improve.</h1>
+        <h3 class="text-white mb-3">Welcome to the Artist Hub</h3>
+        <hr style="color: white">
+        <p class="text-white mb-4" style="text-align: justify; text-justify: inter-word; width: 100%">
+          Fresh, organic picks out of the garden, whose sole purpose is to please your sonic portals to the world.
+          Creative juices sizzling out of excitement, unable to contain themselves any longer, just for you.
+        </p>
+        <p class="text-white text-center mt-5 fw-light" style="font-style: italic">
+          Let's celebrate this moment, where hearts touch and souls connect
+          <br>- sincerely, wiki.
+        </p>
       </div>
-      <div class="doHover2">
-        <i class="bi bi-book text-white h1 mx-2"/>
-      </div>
-      <div class="doHover3">
-        <i class="bi bi-lightning text-white h1 mx-2"/>
+      <!-- Artists -->
+      <div>
+        <div id="indie" class="mt-3">
+          <p class="h3 fw-bold text-white text-center">Indie / Indie Rock</p>
+          <hr style="color: white">
+          <div>
+            <!-- Graffiti Falls -->
+            <div class="card-subtitle text-white">
+              <div class="card-body">
+                <h3 class="card-title fw-bold text-light">
+                  Graffiti Falls
+                </h3>
+                <p class="card-text cardtext">
+                  Band
+                </p>
+                <div class="card-footer">
+                  <button title="Artist Page"
+                          class="btn bi-person btn-lg muArrow" style="color: white;"
+                          v-on:click="gotoArtist('graffitifalls')"><span class="ms-2 small">Artist Page</span></button>
+                  <br>
+                  <button title="SoundCloud"
+                          class="btn bi-cloud-fill btn-lg muArrow" style="color: white;"
+                          v-on:click="redirectSoundcloudGraffitiFalls"><span class="ms-2 small">SoundCloud</span>
+                  </button>
+                  <button title="Youtube"
+                          class="btn bi-youtube btn-lg muArrow" style="color: white;"
+                          v-on:click="redirectYoutubeGraffitiFalls"><span class="ms-2 small">Youtube</span></button>
+                  <button title="Instagram"
+                          class="btn bi-instagram btn-lg muArrow" style="color: white;"
+                          v-on:click="redirectInstagramGraffitiFalls"><span class="ms-2 small">Instagram</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <!-- CLOUDDAY -->
+            <div class="card-subtitle text-white mt-2">
+              <div class="card-body">
+                <h3 class="card-title fw-bold text-light">
+                  CLOUDDAY
+                </h3>
+                <p class="card-text cardtext">
+                  Singer-Songwriter, Producer
+                </p>
+                <p class="card-footer">
+                  <button title="SoundCloud"
+                          class="btn bi-cloud-fill btn-lg muArrow" style="color: white;"
+                          v-on:click="redirectSoundcloudCloudday"><span class="ms-2 small">SoundCloud</span></button>
+                  <button title="Spotify"
+                          class="btn btn-lg muArrow" style="color: white;"
+                          v-on:click="redirectSpotifyCloudday">
+                    <img src="../assets/spotify/Spotify_Icon_RGB_White.png" alt="Spotify"
+                         style="width: 20px; height: 20px" class="mb-1"/> Spotify
+                  </button>
+                  <button title="Instagram"
+                          class="btn bi-instagram btn-lg muArrow" style="color: white;"
+                          v-on:click="redirectInstagramCloudday"><span class="ms-2 small">Instagram</span></button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="hiphop" class="mt-3">
+          <p class="h3 fw-bold text-white text-center">Hip-Hop</p>
+          <hr style="color: white">
+          <!-- Casper: TFD -->
+          <div class="card-subtitle text-white mt-2">
+            <div class="card-body">
+              <h3 class="card-title fw-bold text-light">
+                Casper: TFD
+              </h3>
+              <p class="card-text cardtext">
+                Singer-Songwriter, Producer
+              </p>
+              <p class="card-footer">
+                <button title="SoundCloud"
+                        class="btn bi-cloud-fill btn-lg muArrow" style="color: white;"
+                        v-on:click="redirectSoundcloudCasper"><span class="ms-2 small">SoundCloud</span></button>
+                <button title="Spotify"
+                        class="btn btn-lg muArrow" style="color: white;"
+                        v-on:click="redirectSpotifyCasper">
+                  <img src="../assets/spotify/Spotify_Icon_RGB_White.png" alt="Spotify"
+                       style="width: 20px; height: 20px" class="mb-1"/> Spotify
+                </button>
+                <button title="Youtube"
+                        class="btn bi-youtube btn-lg muArrow" style="color: white;"
+                        v-on:click="redirectYoutubeCasper"><span class="ms-2 small">Youtube</span></button>
+                <button title="Instagram"
+                        class="btn bi-instagram btn-lg muArrow" style="color: white;"
+                        v-on:click="redirectInstagramCasper"><span class="ms-2 small">Instagram</span></button>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <h1 class="text-white mb-4 fw-bold">Discover. Learn. Improve.</h1>
-    <h3 class="text-white mb-3">Welcome to the Artist Hub</h3>
-    <p class="text-light mb-4" style="text-align: justify; text-justify: inter-word; width: 100%">
-      Fresh, organic picks out of the garden, whose sole purpose is to please your sonic portals to the world.<br>
-      Creative juices sizzling out of excitement, unable to contain themselves any longer, just for you.<br>
-      Let's celebrate this moment, where hearts touch and souls connect.
-    </p>
-    <p class="text-light mb-4">Art at your service:</p>
-    <!-- Artists -->
-    <button class="btn btn-outline-light btn-lg muArrow fw-bold mt-3"
-            v-on:click="gotoArtist('graffitifalls')"
-            style="width: 200px; height: 50px">
-      Graffiti Falls
-    </button>
   </div>
 </template>
 
@@ -35,6 +131,126 @@ export default {
   methods: {
     gotoArtist (artist) {
       this.$router.push('/artists/' + artist)
+    },
+    async sendUsageData (usageObj) {
+      const headers = new Headers()
+      headers.set('Authorization', 'Bearer ' + this.$store.state.token)
+      headers.set(
+        'Content-Type', 'application/json'
+      )
+      fetch(
+        'http://localhost:8000/api/utr',
+        {
+          method: 'post',
+          headers: headers,
+          body: JSON.stringify(usageObj)
+        }
+      ).then(r => console.log(r))
+    },
+    redirectSoundcloudGraffitiFalls () {
+      if (this.usageTracker) {
+        this.sendUsageData({
+          source: 'web',
+          module: 'artists',
+          action: 'redirectSoundcloudGraffitiFalls'
+        })
+      }
+      window.open('https://soundcloud.com/graffiti_falls')
+    },
+    redirectInstagramGraffitiFalls () {
+      if (this.usageTracker) {
+        this.sendUsageData({
+          source: 'web',
+          module: 'artists',
+          action: 'redirectInstagramGraffitiFalls'
+        })
+      }
+      window.open('https://www.instagram.com/graffitifallsband/')
+    },
+    redirectYoutubeGraffitiFalls () {
+      if (this.usageTracker) {
+        this.sendUsageData({
+          source: 'web',
+          module: 'artists',
+          action: 'redirectYoutubeGraffitiFalls'
+        })
+      }
+      window.open('https://www.youtube.com/channel/UCf28Jwvrdb6Qeu8t--rBk2Q')
+    },
+    redirectInstagramCloudday () {
+      if (this.usageTracker) {
+        this.sendUsageData({
+          source: 'web',
+          module: 'artists',
+          action: 'redirectInstagramCloudday'
+        })
+      }
+      window.open('https://www.instagram.com/cloudday/')
+    },
+    redirectSoundcloudCloudday () {
+      if (this.usageTracker) {
+        this.sendUsageData({
+          source: 'web',
+          module: 'artists',
+          action: 'redirectSoundcloudCloudday'
+        })
+      }
+      window.open('https://soundcloud.com/cloudvx')
+    },
+    redirectSpotifyCloudday () {
+      if (this.usageTracker) {
+        this.sendUsageData({
+          source: 'web',
+          module: 'artists',
+          action: 'redirectSpotifyCloudday'
+        })
+      }
+      window.open('https://open.spotify.com/artist/56NVjfoRlGzoUWiZxrqj0P?si=YXgxJhWCS8WYMFsSyBTCtw')
+    },
+    redirectInstagramCasper () {
+      if (this.usageTracker) {
+        this.sendUsageData({
+          source: 'web',
+          module: 'artists',
+          action: 'redirectInstagramCasper'
+        })
+      }
+      window.open('https://www.instagram.com/caspertfd/')
+    },
+    redirectSoundcloudCasper () {
+      if (this.usageTracker) {
+        this.sendUsageData({
+          source: 'web',
+          module: 'artists',
+          action: 'redirectSoundcloudCasper'
+        })
+      }
+      window.open('https://soundcloud.com/casperthefriendlydemon')
+    },
+    redirectSpotifyCasper () {
+      if (this.usageTracker) {
+        this.sendUsageData({
+          source: 'web',
+          module: 'artists',
+          action: 'redirectSpotifyCasper'
+        })
+      }
+      window.open('https://open.spotify.com/artist/56Zt5OmCumfVBvxf2JAqEU')
+    },
+    redirectYoutubeCasper () {
+      if (this.usageTracker) {
+        this.sendUsageData({
+          source: 'web',
+          module: 'artists',
+          action: 'redirectYoutubeCasper'
+        })
+      }
+      window.open('https://www.youtube.com/c/CablizzCTFD')
+    }
+  },
+  computed: {
+    usageTracker () {
+      return this.$store.state.usageTracking
     }
   }
 }
@@ -50,16 +266,6 @@ export default {
 
 .muArrow:hover {
   top: -8px;
-}
-
-.supercenter {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin: 0 0 0 -175px;
-  -webkit-transform: translateY(-50%);
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
 }
 
 .doHover {
@@ -108,6 +314,31 @@ export default {
   100% {
     transform: translateY(0);
   }
+}
+
+.wrapper {
+  display: grid;
+  gap: 4em;
+  grid-auto-rows: minmax(100px, auto);
+}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {
+  .wrapper {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+
+/* Medium devices (landscape tablets, 768px and up) */
+@media only screen and (min-width: 768px) {
+  .wrapper {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+.cardtext {
+  font-weight: lighter;
+  font-style: italic;
 }
 
 </style>
